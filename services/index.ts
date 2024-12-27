c69def255d82f9d65735eb64521eb50ecb41cab2
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-27 10:28:03 +0100

    Add error handling to APIGetCourses function for improved reliability

--- services/index.ts
+++ services/index.ts
@@ -1,11 +1,15 @@
 import $http from "./axios.config";
 
-export const APIGetCourses = async (query: string) => {
-	const response = await $http.get("/courses", {
-		params: {
-			search: query,
-		},
-	});
+export const APIGetCourses = async (query: string): Promise<SearchResponse | undefined> => {
+	try {
+		const response = await $http.get("/courses", {
+			params: {
+				search: query,
+			},
+		});
 
-	return response.data;
+		return response.data;
+	} catch (error) {
+		console.error("API Error: ", error);
+	}
 };
