d266ea76573cc3eec68b9169362250d74f8919a6
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-30 12:13:20 +0100

    Add wishlist tab to navigation layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,6 +41,13 @@ export default function TabLayout() {
 					tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
 				}}
 			/>
+			<Tabs.Screen
+				name="wishlist"
+				options={{
+					title: "Wishlist",
+					tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+				}}
+			/>
 		</Tabs>
 	);
 }
