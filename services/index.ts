bf27ddd1cae8915c318b5a00544f999eadf27fec
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-30 12:14:36 +0100

    Add API functions to retrieve course details, curriculum, and reviews

--- services/index.ts
+++ services/index.ts
@@ -13,3 +13,46 @@ export const APIGetCourses = async (query: string): Promise<SearchResponse | und
 		console.error("API Error: ", error);
 	}
 };
+
+export const APIGetCourseById = async (courseId: string): Promise<Course | undefined> => {
+	try {
+		const response = await $http.get(`/courses/${courseId}`);
+
+		return response.data;
+	} catch (error) {
+		console.error("API Error: ", error);
+	}
+};
+
+export const APIGetRecommendedCourses = async (): Promise<SearchResponse | undefined> => {
+	try {
+		const response = await $http.get("/courses");
+
+		return response.data;
+	} catch (error) {
+		console.error("API Error: ", error);
+	}
+};
+
+export const APIGetCourseCurriculum = async (
+	courseId: string,
+	page: number
+): Promise<CurriculumResponse | undefined> => {
+	try {
+		const response = await $http.get(`/courses/${courseId}/public-curriculum-items/?page=${page}`);
+
+		return response.data;
+	} catch (error) {
+		console.error("API Error: ", error);
+	}
+};
+
+export const APIGetCourseReviews = async (courseId: string): Promise<ReviewsResponse | undefined> => {
+	try {
+		const response = await $http.get(`/courses/${courseId}/reviews`);
+
+		return response.data;
+	} catch (error) {
+		console.error("API Error: ", error);
+	}
+};
